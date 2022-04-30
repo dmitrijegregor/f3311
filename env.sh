c11647986e49d501fdfbee28e806a03c6ad05430
dmitrijegregor <104157328+dmitrijegregor@users.noreply.github.com>
2022-05-01 01:01:34 +1100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PATH=$PWD/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
+PATH=/home/runner/work/f3311/f3311/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:/home/runner/work/f3311/f3311/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8/bin:/home/runner/work/f3311/f3311/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
 export PATH
 
