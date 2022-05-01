74f415983a54a1bf4afd61a425ea2b32675e5c13
dmitrijegregor <104157328+dmitrijegregor@users.noreply.github.com>
2022-05-02 00:52:59 +1100

    Update hello.c

--- МуДр/привет/hello.c
+++ МуДр/привет/hello.c
@@ -1,14 +1,32 @@
-#include <linux/init.h> 
-#include <linux/module.h> 
-#include <linux/kernel.h> 
-MODULE_LICENSE(“GPL”); 
-MODULE_AUTHOR(“Robert W. Oliver II”); 
-MODULE_DESCRIPTION(“A simple example Linux module.”); 
-MODULE_VERSION(“0.01”); 
-static int __init lkm_example_init(void) 
-{ printk(KERN_INFO “Hello, World!\n”); 
-return 0; } 
-static void __exit lkm_example_exit(void) 
-{ printk(KERN_INFO “Goodbye, World!\n”); } 
-module_init(lkm_example_init); 
-module_exit(lkm_example_exit);
+#include <linux/module.h>
+#include <linux/kallsyms.h>
+#include <linux/uaccess.h>
+#include "find.c"
+
+/* <linux/syscalls.h>
+asmlinkage long sys_write( unsigned int fd, 
+                           const char __user *buf,
+                           size_t count ); */
+
+static asmlinkage long (*sys_write) (
+   unsigned int, const char __user *, size_t );
+
+static int __init wr_init( void ) {
+   char buf[ 80 ] = "Hello from kernel!\n";
+   int len = strlen( buf ), n;
+   sys_write = find_sym( "sys_write" );    
+   printk( "+ sys_write address = %p\n", sys_write );
+   printk( "+ [%d]: %s", len, buf );
+   if( sys_write != NULL ) {
+      mm_segment_t fs = get_fs();
+      set_fs( get_ds() );
+      n = sys_write( 1, buf, len );
+      set_fs( fs );
+      printk( "+ printf() return : %d\n", n );
+   }
+   return -EPERM; 
+}
+
+module_init( wr_init );
+MODULE_LICENSE( "GPL" );
+MODULE_AUTHOR( "Oleg Tsiliuric <olej@front.ru>" );
